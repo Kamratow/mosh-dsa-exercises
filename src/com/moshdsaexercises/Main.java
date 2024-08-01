d9b0cbd4e9cffd9bfab8fc2e7f2b7391153a88ac
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-01 16:57:28 +0200

    Added removeAt method usage

--- src/com/moshdsaexercises/Main.java
+++ src/com/moshdsaexercises/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("Max item value: " + numbers.max());
         System.out.println("Printing all items from array:");
         numbers.print();
+        numbers.removeAt(3);
     }
 }
